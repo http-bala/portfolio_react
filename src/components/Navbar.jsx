4f8fe57e2cfb7a3c760e952078898a7c74fb00d3
Balakrishna kolla <balakrishnakolla00014@gmail.com>
2024-05-25 01:40:24 +0530

    fix/ fix small bugs and issus

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [Nav, setNav] = useState(true)
 
   const handleClick = () => {
-   setNav(false)
+    setNav(false)
   }
   const handlecloseClick = () => {
     setNav(true)
@@ -19,20 +19,20 @@ const Navbar = () => {
           Balakrishna
         </span>
         <span className='cursor-pointer md:hidden block'>
-        {
-          Nav ? 
-            <FaBars color='white' onClick={handleClick} />
-          : 
-            <FaTimes  color='white' onClick={handlecloseClick} />
-        }
+          {
+            Nav ?
+              <FaBars color='white' onClick={handleClick} />
+              :
+              <FaTimes color='white' onClick={handlecloseClick} />
+          }
         </span>
       </div>
-      <ul className={`md:flex md:items-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 bg-gradient-to-b from-slate-900 to-purple-900 sm:bg-none ${Nav ? "top-[-400px]" : "top-[70px] " }   transition-all ease-in duration-500`}>
-       <AnchorLink href='#home'> <li className='text-xl hover:text-white text-purple-500 duration-500 mx-5 my-6 md:y-0'>Home</li> </AnchorLink>
-       <AnchorLink href='#about'> <li className='text-xl hover:text-white  text-purple-500 duration-500 mx-5 my-6 md:y-0'>About</li> </AnchorLink>
-       <AnchorLink href='#service'> <li className='text-xl hover:text-white text-purple-500 duration-500 mx-5 my-6 md:y-0'>Service</li></AnchorLink>
-       <AnchorLink href='#project'> <li className='text-xl hover:text-white text-purple-500 duration-500 mx-5 my-6 md:y-0'>Project</li></AnchorLink>
-       <AnchorLink href='#contact'> <li className='text-xl hover:text-white text-purple-500 duration-500 mx-5 my-6 md:y-0'>Contact</li></AnchorLink>
+      <ul className={`md:flex md:items-center z-[1] md:z-auto md:static absolute w-full left-0 md:w-auto md:py-0 py-4 md:pl-0 pl-7 bg-gradient-to-b from-slate-900 to-purple-900 md:bg-none ${Nav ? "top-[-400px]" : "top-[70px] "}   transition-all ease-in duration-500`}>
+        <AnchorLink href='#home'> <li className='text-xl hover:text-white text-purple-500 duration-500 mx-5 my-6 md:y-0'>Home</li> </AnchorLink>
+        <AnchorLink href='#about'> <li className='text-xl hover:text-white  text-purple-500 duration-500 mx-5 my-6 md:y-0'>About</li> </AnchorLink>
+        <AnchorLink href='#service'> <li className='text-xl hover:text-white text-purple-500 duration-500 mx-5 my-6 md:y-0'>Service</li></AnchorLink>
+        <AnchorLink href='#project'> <li className='text-xl hover:text-white text-purple-500 duration-500 mx-5 my-6 md:y-0'>Project</li></AnchorLink>
+        <AnchorLink href='#contact'> <li className='text-xl hover:text-white text-purple-500 duration-500 mx-5 my-6 md:y-0'>Contact</li></AnchorLink>
       </ul>
     </nav>
   )
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -8,7 +8,7 @@ const Services = () => {
   return (
     <div className=' sm:flex items-center justify-center max-w-screen-xl gap-5 m-auto px-5 mb-10' id="service">
        {/* service */}
-       <div className="sm:p-10 p-5 mb-7  rounded-lg bg-white/10 shadow-lg text-white flex flex-col items-center ">
+       <div className="sm:p-2 md:p-10 p-5 mb-7  rounded-lg bg-white/10 shadow-lg text-white flex flex-col items-center ">
        <SiExcalidraw size={50} />
          <div className="mt-8">
             <h3 className='font-bold text-xl text-center'>Web-designing</h3>
@@ -17,7 +17,7 @@ const Services = () => {
       </div>
       {/* service */}
        {/* service */}
-       <div className="sm:p-10 p-5 mb-7  rounded-lg bg-white/10 shadow-lg text-white flex flex-col items-center">
+       <div className="sm:p-2 md:p-10 p-5 mb-7  rounded-lg bg-white/10 shadow-lg text-white flex flex-col items-center">
        <ImFilePicture size={50} />
          <div className="mt-8">
             <h3 className='font-bold text-xl text-center'>Web-designing</h3>
@@ -26,7 +26,7 @@ const Services = () => {
       </div>
       {/* service */}
        {/* service */}
-       <div className="sm:p-10 p-5 mb-7  rounded-lg bg-white/10 shadow-lg text-white flex flex-col items-center">
+       <div className="sm:p-2 md:p-10 p-5 mb-7  rounded-lg bg-white/10 shadow-lg text-white flex flex-col items-center">
        <TbSeo size={50} />
          <div className="mt-8">
             <h3 className='font-bold text-xl text-center'>Web-designing</h3>
