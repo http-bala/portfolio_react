8e0c4ccfd6b32b0e95b463877ac61d9b40c5a438
Balakrishna kolla <balakrishnakolla00014@gmail.com>
2024-05-26 08:44:27 +0530

    changes/ on services , project/feature/added hover effect and link

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,57 +1,93 @@
 import React from 'react'
+import image from './assets/project_image/image.png'
+import image1 from './assets/project_image/image1.png'
+import image2 from './assets/project_image/image2.png'
+import image3 from './assets/project_image/image3.png'
+import image4 from './assets/project_image/image4.png'
+import image5 from './assets/project_image/image5.png'
 
 const Project = () => {
     return (
         <div className='max-w-screen-xl m-auto' id="project">
             <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
-                <div class="-m-1 flex flex-wrap md:-m-2">
-                    <div class="flex w-1/3 flex-wrap">
-                        <div class="w-full p-1 md:p-2">
-                            <img
-                                alt="gallery"
-                                class="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp" />
-                        </div>
+                <div class="sm:flex flex-wrap md:-m-2">
+                    <div class="flex sm:w-1/3 flex-wrap">
+                        <a href="https://enlighteneddigital.in/maa/Maa_Packageing_home">
+                            <div class="w-full h-[250px] p-1 md:p-2 relative group">
+                                <img
+                                    alt="gallery"
+                                    class="block h-full w-full rounded-lg object-cover object-left"
+                                    src={image} />
+                                <div className='absolute inset-1 md:inset-2  bg-black bg-opacity-70 opacity-0 transition-opacity flex flex-col items-center justify-center rounded-lg group-hover:opacity-100'>
+                                    <h1 className='text-white text-3xl'>PHP STATIC WEBSITE</h1>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                    <div class="flex w-1/3 flex-wrap">
-                        <div class="w-full p-1 md:p-2">
-                            <img
-                                alt="gallery"
-                                class="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp" />
-                        </div>
+                    <div class="flex sm:w-1/3 flex-wrap">
+                        <a href="https://canaknaturecare.in/">
+                            <div class="w-full  h-[250px] p-1 md:p-2 relative group">
+                                <img
+                                    alt="gallery"
+                                    class="block h-full w-full rounded-lg object-cover object-center"
+                                    src={image1} />
+                                <div className='absolute inset-1 md:inset-2  bg-purple-900 bg-opacity-40 opacity-0 transition-opacity flex items-center justify-center rounded-lg group-hover:opacity-100'>
+                                <h1 className='text-white text-2xl'>Wordpress E-COMMERCE</h1>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                    <div class="flex w-1/3 flex-wrap">
-                        <div class="w-full p-1 md:p-2">
-                            <img
-                                alt="gallery"
-                                class="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp" />
-                        </div>
+                    <div class="flex sm:w-1/3 flex-wrap">
+                        <a href="https://github.com/http-bala/netfilx-clone">
+                            <div class="w-full h-[250px] p-1 md:p-2 relative group">
+                                <img
+                                    alt="gallery"
+                                    class="block h-full w-full rounded-lg object-cover object-center"
+                                    src={image2} />
+                                <div className='absolute inset-1 md:inset-2  bg-purple-900 bg-opacity-40 opacity-0 transition-opacity flex items-center justify-center rounded-lg group-hover:opacity-100'>
+                                <h1 className='text-white text-3xl'>NETFLIX CLONE</h1>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                    <div class="flex w-1/3 flex-wrap">
-                        <div class="w-full p-1 md:p-2">
-                            <img
-                                alt="gallery"
-                                class="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp" />
-                        </div>
+                    <div class="flex sm:w-1/3 flex-wrap">
+                        <a href="https://github.com/http-bala/BCA_finalYearProject">
+                            <div class="w-full h-[250px] p-1 md:p-2 relative group">
+                                <img
+                                    alt="gallery"
+                                    class="block h-full w-full rounded-lg object-fit object-center"
+                                    src={image3} />
+                                <div className='absolute inset-1 md:inset-2  bg-purple-900 bg-opacity-40 opacity-0 transition-opacity flex items-center justify-center rounded-lg group-hover:opacity-100'>
+                                <h1 className='text-white text-3xl text-center'>FIELD VIST MANAGEMENT SYSYTEM</h1>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                    <div class="flex w-1/3 flex-wrap">
-                        <div class="w-full p-1 md:p-2">
-                            <img
-                                alt="gallery"
-                                class="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp" />
-                        </div>
+                    <div class="flex sm:w-1/3 flex-wrap">
+                        <a href="https://deluxe-cheesecake-9ca38c.netlify.app/">
+                            <div class="w-full  h-[250px] p-1 md:p-2 relative group">
+                                <img
+                                    alt="gallery"
+                                    class="block h-full w-full rounded-lg object-cover object-center"
+                                    src={image4} />
+                                <div className='absolute inset-1 md:inset-2  bg-purple-900 bg-opacity-40 opacity-0 transition-opacity flex items-center justify-center rounded-lg group-hover:opacity-100'>
+                                <h1 className='text-white text-3xl text-center'>SPOTIFY CLONE</h1>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                    <div class="flex w-1/3 flex-wrap">
-                        <div class="w-full p-1 md:p-2">
-                            <img
-                                alt="gallery"
-                                class="block h-full w-full rounded-lg object-cover object-center"
-                                src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp" />
-                        </div>
+                    <div class="flex sm:w-1/3 flex-wrap">
+                        <a href="https://github.com/http-bala/Tvflix_php">
+                            <div class="w-full p-1  h-[250px] md:p-2 relative group">
+                                <img
+                                    alt="gallery"
+                                    class="block h-full w-full rounded-lg object-cover object-center"
+                                    src={image5} />
+                                <div className='absolute inset-1 md:inset-2  bg-purple-900 bg-opacity-40 opacity-0 transition-opacity flex items-center justify-center rounded-lg group-hover:opacity-100'>
+                                <h1 className='text-white text-3xl text-center '>VIDEO STREAMING WEBSITE</h1>
+                                </div>
+                            </div>
+                        </a>
                     </div>
                 </div>
             </div>
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,46 +1,41 @@
 import React from 'react'
-import { SiExcalidraw } from "react-icons/si";
-import { ImFilePicture } from "react-icons/im";
-import { TbSeo } from "react-icons/tb";
+import { MdDesignServices } from "react-icons/md";
+import { MdSettingsApplications } from "react-icons/md";
+import { FaServer } from "react-icons/fa";
 
 
 const Services = () => {
-  return (
-    <div className=' sm:flex items-center justify-center max-w-screen-xl gap-5 m-auto px-5 mb-10' id="service">
-       {/* service */}
-       <div className="sm:p-2 md:p-10 p-5 mb-7  rounded-lg bg-white/10 shadow-lg text-white flex flex-col items-center ">
-       <SiExcalidraw size={50} />
-         <div className="mt-8">
-            <h3 className='font-bold text-xl text-center'>Web-designing</h3>
-            <p className='text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, facere.</p>
+   return (
+      <div className=' sm:flex items-center justify-center max-w-screen-xl gap-5 m-auto px-5 mb-10' id="service">
+         {/* service */}
+         <div className="sm:p-2 md:p-4 p-5 mb-7 flex-1 h-[100%] rounded-lg bg-white/20 shadow-lg text-white flex flex-col items-center ">
+            <MdDesignServices size={50} />
+            <div className="mt-8">
+               <h3 className='font-bold text-xl text-center'>Web-designing</h3>
+               <p className='text-center'>"Designing visually appealing and user-friendly websites is my passion. I craft sites that not only look great but also offer a seamless user experience, tailored to your brand and goals."</p>
+            </div>
          </div>
-      </div>
-      {/* service */}
-       {/* service */}
-       <div className="sm:p-2 md:p-10 p-5 mb-7  rounded-lg bg-white/10 shadow-lg text-white flex flex-col items-center">
-       <ImFilePicture size={50} />
-         <div className="mt-8">
-            <h3 className='font-bold text-xl text-center'>Web-designing</h3>
-            <p className='text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, facere.</p>
+         {/* service */}
+         {/* service */}
+         <div className="sm:p-2 md:p-4 p-5 mb-7 flex-1 h-[100%] rounded-lg bg-white/20 shadow-lg text-white flex flex-col items-center">
+            <MdSettingsApplications size={50} />
+            <div className="mt-8">
+               <h3 className='font-bold text-xl text-center'>Web based Application</h3>
+               <p className='text-center'> web-based applications that are tailored to meet your business needs.A simple content management systems to complex business applications, I provide solutions that are scalable, secure, and efficient</p>
+            </div>
          </div>
-      </div>
-      {/* service */}
-       {/* service */}
-       <div className="sm:p-2 md:p-10 p-5 mb-7  rounded-lg bg-white/10 shadow-lg text-white flex flex-col items-center">
-       <TbSeo size={50} />
-         <div className="mt-8">
-            <h3 className='font-bold text-xl text-center'>Web-designing</h3>
-            <p className='text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates, facere.</p>
+         {/* service */}
+         {/* service */}
+         <div className="sm:p-2 md:p-4 p-5 mb-7 flex-1 h-[100%] rounded-lg bg-white/20 shadow-lg text-white flex flex-col items-center">
+            <FaServer size={50} />
+            <div className="mt-8">
+               <h3 className='font-bold text-xl text-center'>backend development</h3>
+               <p className='text-center'>Strong backend systems are the backbone of any successful web application. I specialize in building reliable and efficient backends that ensure your application runs smoothly.</p>
+            </div>
          </div>
+         {/* service */}
       </div>
-      {/* service */}
-      
-      
-       
-
-
-    </div>
-  )
+   )
 }
 
 export default Services
