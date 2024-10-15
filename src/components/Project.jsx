83cd7f9a519c37b0a46d6d39816db3736d330e2e
Balakrishna kolla <138421288+http-bala@users.noreply.github.com>
2024-10-15 17:16:25 +0530

    my project remove

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -11,7 +11,7 @@ const Project = () => {
         <div className='max-w-screen-xl m-auto' id="project">
             <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
                 <div class="sm:flex flex-wrap md:-m-2">
-                    <div class="flex sm:w-1/3 flex-wrap">
+                    {/* <div class="flex sm:w-1/3 flex-wrap">
                         <a href="https://enlighteneddigital.in/maa/Maa_Packageing_home">
                             <div class="w-full h-[250px] p-1 md:p-2 relative group">
                                 <img
@@ -23,8 +23,8 @@ const Project = () => {
                                 </div>
                             </div>
                         </a>
-                    </div>
-                    <div class="flex sm:w-1/3 flex-wrap">
+                    </div> */}
+                    {/* <div class="flex sm:w-1/3 flex-wrap">
                         <a href="https://canaknaturecare.in/">
                             <div class="w-full  h-[250px] p-1 md:p-2 relative group">
                                 <img
@@ -36,7 +36,7 @@ const Project = () => {
                                 </div>
                             </div>
                         </a>
-                    </div>
+                    </div> */}
                     <div class="flex sm:w-1/3 flex-wrap">
                         <a href="https://github.com/http-bala/netfilx-clone">
                             <div class="w-full h-[250px] p-1 md:p-2 relative group">
